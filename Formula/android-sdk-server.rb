3a2c687c551ee030402ff7299896d970bf3a5248
İbrahim Süren <ibrahimsn98@gmail.com>
2025-06-25 13:19:16 +0300

    update formula

--- Formula/android-sdk-server.rb
+++ Formula/android-sdk-server.rb
@@ -2,7 +2,7 @@ class AndroidSdkServer < Formula
   desc "Service to manage Android SDK tasks over a server"
   homepage "https://github.com/ibrahimsn98/android-sdk-server"
   url "https://github.com/ibrahimsn98/android-sdk-server/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "91aa23eb40155ecf07f195d34a634ca5dcfde6677eb641e640fd57a22ebf6bfa"
+  sha256 "8f42e29fedca2488c202cfdd3e029604203489ad1bbc71627018871ed2b335bf"
   license "Apache"
 
   depends_on "go" => :build
